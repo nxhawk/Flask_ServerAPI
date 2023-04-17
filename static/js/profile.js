01abe8292452fb5819ff9652a3b289d239636000
haonhat0703 <haonhat2729@gmail.com>
2023-04-17 23:19:06 +0700

    handle show profile and my post

---
+++ static/js/profile.js
@@ -0,0 +1,48 @@
+const API =
+  location.protocol +
+  "//" +
+  document.domain +
+  ":" +
+  location.port +
+  "/api/getprofile";
+let user;
+const getProfile = async () => {
+  try {
+    await fetch(API, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data || data.status == false) throw new Error();
+        user = data.message;
+        document.getElementById("load").style.display = "none";
+        Funcmain();
+      });
+  } catch (error) {
+    console.log(error.message);
+    window.location.href = "/api/login";
+  }
+};
+
+getProfile();
+
+//----------------------After check token -------------------
+// main func
+
+function Funcmain() {
+  let main = document.querySelector("main");
+  main.innerHTML += `
+    <div style = "font-size:25px;">Hello <span style = "color:red;">${user.username}</span>, Your email <span style = "color:orange;">${user.email}</span></div>
+    
+    <button id = "logout">Logout</button>
+  `;
+  //--------------- Button click=> logout------------------------
+  document.getElementById("logout").addEventListener("click", () => {
+    localStorage.clear();
+    window.location.href = "/api/login";
+  });
+}
