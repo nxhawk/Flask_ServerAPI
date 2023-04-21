72dd66db59f36273731542963e80bbc11b77ff2f
haonhat0703 <haonhat2729@gmail.com>
2023-04-21 10:09:26 +0700

    add all my posts

--- static/js/profile.js
+++ static/js/profile.js
@@ -5,6 +5,14 @@ const API =
   ":" +
   location.port +
   "/api/getprofile";
+const API2 =
+  location.protocol +
+  "//" +
+  document.domain +
+  ":" +
+  location.port +
+  "/api/v1/getposts";
+
 let user;
 const getProfile = async () => {
   try {
@@ -33,13 +41,32 @@ getProfile();
 //----------------------After check token -------------------
 // main func
 
-function Funcmain() {
+async function Funcmain() {
   let main = document.querySelector("main");
   main.innerHTML += `
     <div style = "font-size:25px;">Hello <span style = "color:red;">${user.username}</span>, Your email <span style = "color:orange;">${user.email}</span></div>
     
     <button id = "logout">Logout</button>
   `;
+  try {
+    await fetch(API2, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data || data.status == false) throw new Error();
+        posts = data.posts;
+        for (post of posts) {
+          main.innerHTML += `<div><span>${post.post_name}</span> ${post.description}</div>`;
+        }
+      });
+  } catch (error) {
+    console.log(error.message);
+  }
   //--------------- Button click=> logout------------------------
   document.getElementById("logout").addEventListener("click", () => {
     localStorage.clear();
